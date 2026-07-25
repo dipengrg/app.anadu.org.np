f55371e572b96ad33da0edfd34ed6e12fc459caf
Dipendra Gurung <dipugurung@gmail.com>
2026-07-25 09:03:47 +0000

    Fix: Unused codes.

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\Api\V1\UserManagementController;
 
 /**
  * API Routes for Mobile App Authentication
@@ -23,14 +22,5 @@ Route::prefix('v1')->group(function () {
     // Authenticated Session Endpoints (Requires Bearer Token)
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/profile', [AuthController::class, 'profile']);
-
-        // Only Admins can manage users
-        Route::middleware('role:admin')->group(function () {
-            Route::get('/users', [UserManagementController::class, 'index']);
-            Route::get('/users/{user}', [UserManagementController::class, 'show']);
-            Route::post('/users', [UserManagementController::class, 'store']);
-            Route::put('/users/{user}', [UserManagementController::class, 'update']);
-            Route::delete('/users/{user}', [UserManagementController::class, 'destroy']);
-        });
     });
 });
