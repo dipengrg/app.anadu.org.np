16be5ea43417e20d145067b502017b2f846b22ae
Dipendra Gurung <dipugurung@gmail.com>
2026-07-26 09:11:27 +0000

    Fixed: User (moderator) management.

--- app/Filament/Resources/Users/Pages/ManageUsers.php
+++ app/Filament/Resources/Users/Pages/ManageUsers.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Filament\Resources\UserResource\Pages;
+namespace App\Filament\Resources\Users\Pages;
 
-use App\Filament\Resources\UserResource;
+use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\CreateAction;
-use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ManageRecords;
 
-class ListUsers extends ListRecords
+class ManageUsers extends ManageRecords
 {
     protected static string $resource = UserResource::class;
 
