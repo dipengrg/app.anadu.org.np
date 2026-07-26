5639ca1f07c81920e3f3973bbe11a23d3950bc26
Dipendra Gurung <dipugurung@gmail.com>
2026-07-26 14:27:32 +0000

    Feat: Event session management.

---
+++ app/Filament/Resources/EventSessions/EventSessionResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources\EventSessions;
+
+use App\Filament\Resources\EventSessions\Pages\ManageEventSessions;
+use App\Models\EventSession;
+use UnitEnum;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class EventSessionResource extends Resource
+{
+    protected static ?string $model = EventSession::class;
+
+    protected static string | UnitEnum | null $navigationGroup = 'Events';
+    protected static ?int $navigationSort = 2;
+    protected static ?string $navigationLabel = 'Session Management';
+
+    protected static ?string $recordTitleAttribute = 'title';
+
+    public static function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('event_id')
+                    ->relationship('event', 'title')
+                    ->required(),
+                TextInput::make('title')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('title')
+            ->columns([
+                TextColumn::make('event.title')
+                    ->searchable(),
+                TextColumn::make('title')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ManageEventSessions::route('/'),
+        ];
+    }
+}
