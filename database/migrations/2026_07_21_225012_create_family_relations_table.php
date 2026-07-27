5f9a9a4f505f2a26dc502ece53f21ca5e1b44175
Dipendra Gurung <dipugurung@gmail.com>
2026-07-27 16:39:50 +0000

    Fix: Family Tree schema updated.

--- database/migrations/2026_07_21_225012_create_family_relations_table.php
+++ database/migrations/2026_07_21_225012_create_family_relations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('family_trees', function (Blueprint $table) {
+        Schema::create('family_relations', function (Blueprint $table) {
             $table->id();
 
             $table->boolean('is_root')->default(false);
