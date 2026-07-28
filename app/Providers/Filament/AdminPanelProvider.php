8ff6ffe8d609fcbcff1e613ece926cb31e60cc8b
Dipendra Gurung <dipugurung@gmail.com>
2026-07-28 03:53:09 +0000

    Fix: Prevents inactive users from logging in.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Http\Middleware\EnsureUserCanAccessPanel;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -73,6 +74,7 @@ class AdminPanelProvider extends PanelProvider
                 DispatchServingFilamentEvent::class,
             ])
             ->authMiddleware([
+                EnsureUserCanAccessPanel::class,
                 Authenticate::class,
             ]);
     }
